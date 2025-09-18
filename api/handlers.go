047ac6b2230ae776f398e85a3553f0b253cce01d
wsprouse <wsprouse@gmail.com>
2025-09-17 22:24:55 -0400

    Adding storage calculation for eventual population on frontend

--- api/handlers.go
+++ api/handlers.go
@@ -3,8 +3,32 @@ package api
 import (
 	"fmt"
 	"net/http"
+	"golang.org/x/sys/unix"
+	"os"
 )
 
 func GetSystemMetrics(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, Go HTTP Server!")
+	"""
+	This function gets system metrics to populate the dashboard with. 
+	These metrics include CPU usage, Memory, and Disk Usage
+
+	This function returns a json payload of the metrics it collects
+	"""
+	wd, err := os.Getwd()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error getting pwd: %v\n", err)
+		return
+	}
+
+	var statfs unix.Statfs_t
+	err = unix.Statfs(wd, &statfs)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error getting statfs for %s: %v\n", wd, err)
+		return
+	}
+
+	freeBytes := statfs.Bavail * uint64(statfs.Bsize)
+
+	fmt.Fprintf(w, "Hello, Go HTTP Server! You have %d bytes avail", freeBytes)
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/WavexSoftware/OpenCloud
 
-go 1.23.12
+go 1.24.0
+
+toolchain go1.24.7
+
+require golang.org/x/sys v0.36.0 // indirect
