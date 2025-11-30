e2692ef7c892fea64eb8e718bad437f6f4c76704
wsprouse <wsprouse@gmail.com>
2025-11-30 11:12:04 -0500

    dynamically detecting runtime

--- api/compute_handlers.go
+++ api/compute_handlers.go
@@ -359,7 +359,7 @@ func addCron(filePath string) error {
 
 	// Cron job to append
 	//newCronJob := "* * * * * echo \"Hello from Go cron!\" >> /tmp/go_cron.log"
-	newCronJob := fmt.Sprintf("* * * * * python %s >> /tmp/go_cron_output.log 2>&1", filePath)
+	newCronJob := fmt.Sprintf("* * * * * %s %s >> /tmp/go_cron_output.log 2>&1", detectRuntime(filePath), filePath)
 
 	// Prevent duplicate entries
 	if strings.Contains(currentCrontab, newCronJob) {
