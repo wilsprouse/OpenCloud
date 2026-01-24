454acfa47c9da7019d2b6ffb3dd784558d613c18
Ubuntu <ubuntu@ip-172-31-197-105.ec2.internal>
2026-01-24 18:59:10 +0000

    Adding setup.sh for installing dev deps

---
+++ setup.sh
@@ -0,0 +1,13 @@
+# This Script installs the necessary software to host the bare minimum of OpenCloud: The User Interface.
+# Consistent with OpenCloud's ideals, the subdependencies for each service are installed when a user enables the service on their cluster
+#
+#
+#
+
+# Install Go (Check if its installed already, if not, install it
+# Install typescript and typescript dependencies for this repo
+# Compile the Go app into a binary
+# Compile the typescript into compiled TypeScript
+# Move Go binary to systemd service
+# Move compiled TypeScript into a service?
+# Start the services
